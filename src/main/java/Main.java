c209a9e777a0d8c8658b6bf8b93d6866bc83ea14
Ferdinand-cmd <120234106+Ferdinand-cmd@users.noreply.github.com>
2023-06-12 19:01:33 +0700

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import Engine.*;
+import Engine.Object;
 import org.joml.*;
 import org.lwjgl.opengl.GL;
 
+import java.io.IOException;
 import java.lang.Math;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,11 +14,14 @@ import static org.lwjgl.opengl.GL30.*;
 
 public class Main {
     private Window window = new Window(1080, 1080, "Hello World");
-    ArrayList<Object2d> objectObj = new ArrayList<>();
+    ArrayList<Object> objectObj = new ArrayList<>();
     Camera camera = new Camera();
     Projection projection = new Projection(window.getWidth(), window.getHeight());
+    float move2 = 1f;
+    boolean press_r = false;
+    float derajatCam_r = 0f;
 
-    public void run() {
+    public void run() throws IOException {
 
         init();
         loop();
@@ -26,57 +31,129 @@ public class Main {
         glfwSetErrorCallback(null).free();
     }
 
-    public void init() {
+    public void init() throws IOException {
         window.init();
         GL.createCapabilities();
         camera.setPosition(0, 0.5f, 2.0f);
         camera.setRotation((float) Math.toRadians(0.0f), (float) Math.toRadians(0.0f));
 
-        objectObj.add(new Object2d(Arrays.asList(
-                new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER),
-                new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)
-        ),
-                new ArrayList<>(),
-                new Vector4f(1f, 1, 0, 1.0f), "resources/model/stall/stall.obj"));
+//        objectObj.add(new Object2d(Arrays.asList(
+//                new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER),
+//                new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)
+//        ),
+//                new ArrayList<>(),
+//                new Vector4f(1f, 1, 0, 1.0f), "resources/model/stall/stall.obj"));
+//
+//        objectObj.get(0).scale(0.5f, 0.5f, 0.5f);
 
-        objectObj.get(0).scale(0.5f, 0.5f, 0.5f);
+        objectObj.add(new Model(
+                Arrays.asList(
+                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER),
+                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER)
+                ),
+                new ArrayList<>(),
+                new Vector4f(1.0f,0.0f,0.0f,1.0f),
+                "resources/model/mobil/mobil.obj"
+        ));
+        objectObj.get(0).scaleObject(0.1f,0.1f,0.1f);
     }
 
-    public void input() {
+    public void input(){
         float move = 0.01f;
         if (window.isKeyPressed(GLFW_KEY_W)) {
+            camera.moveForward(1.7f);
+            camera.addRotation(0, (float)Math.toRadians(-derajatCam_r));
+            camera.moveBackwards(1.7f);
+
+            objectObj.get(0).translateObject(0.0f, 0.0f, -move);
             camera.moveForward(move);
+
+            camera.moveForward(1.7f);
+            camera.addRotation(0, (float)Math.toRadians(derajatCam_r));
+            camera.moveBackwards(1.7f);
         }
-        if (window.isKeyPressed(GLFW_KEY_A)) {
-            camera.moveLeft(move);
-        }
-        if (window.isKeyPressed(GLFW_KEY_S)) {
+        else if (window.isKeyPressed(GLFW_KEY_S)){
+            camera.moveForward(1.7f);
+            camera.addRotation(0, (float)Math.toRadians(-derajatCam_r));
+            camera.moveBackwards(1.7f);
+
+            objectObj.get(0).translateObject(0.0f, 0.0f, move);
             camera.moveBackwards(move);
+
+            camera.moveForward(1.7f);
+            camera.addRotation(0, (float)Math.toRadians(derajatCam_r));
+            camera.moveBackwards(1.7f);
         }
-        if (window.isKeyPressed(GLFW_KEY_D)) {
+        else if (window.isKeyPressed(GLFW_KEY_D)){
+            camera.moveForward(1.7f);
+            camera.addRotation(0, (float)Math.toRadians(-derajatCam_r));
+            camera.moveBackwards(1.7f);
+
+            objectObj.get(0).translateObject(move, 0.0f, 0.0f);
             camera.moveRight(move);
+
+            camera.moveForward(1.7f);
+            camera.addRotation(0, (float)Math.toRadians(derajatCam_r));
+            camera.moveBackwards(1.7f);
         }
-        if (window.getMouseInput().isRightButtonPressed()) {
-            Vector2f displVec = window.getMouseInput().getDisplVec();
-            camera.addRotation((float) Math.toRadians(displVec.x * 0.1f), (float) Math.toRadians(displVec.y * 0.1f));
+        else if (window.isKeyPressed(GLFW_KEY_A)){
+            camera.moveForward(1.7f);
+            camera.addRotation(0, (float)Math.toRadians(-derajatCam_r));
+            camera.moveBackwards(1.7f);
+
+            objectObj.get(0).translateObject(-move, 0.0f, 0.0f);
+            camera.moveLeft(move);
+
+            camera.moveForward(1.7f);
+            camera.addRotation(0, (float)Math.toRadians(derajatCam_r));
+            camera.moveBackwards(1.7f);
         }
-        if (window.isKeyPressed(GLFW_KEY_SPACE)) {
+        if(window.isKeyPressed(GLFW_KEY_UP)){
             camera.moveUp(move);
+            objectObj.get(0).translateObject(0.0f, move, 0.0f);
         }
-        if (window.isKeyPressed(GLFW_KEY_LEFT_SHIFT)) {
+        else if(window.isKeyPressed(GLFW_KEY_DOWN)){
             camera.moveDown(move);
+            objectObj.get(0).translateObject(0.0f, -move, 0.0f);
         }
-        if (window.isKeyPressed(GLFW_KEY_UP)) {
-            camera.addRotation(-0.01f, 0f);
+        else if(window.isKeyPressed(GLFW_KEY_LEFT)){
+            camera.addRotation(0.0f,0.05f);
         }
-        if (window.isKeyPressed(GLFW_KEY_DOWN)) {
-            camera.addRotation(0.01f, 0f);
+        else if(window.isKeyPressed(GLFW_KEY_RIGHT)){
+            camera.addRotation(0.0f,-0.05f);
         }
-        if (window.isKeyPressed(GLFW_KEY_LEFT)) {
-            camera.addRotation(0f, -0.01f);
+        if (window.getMouseInput().isRightButtonPressed()){
+            Vector2f displVec = window.getMouseInput().getDisplVec();
+            camera.addRotation((float) Math.toRadians(displVec.x * 0.1f) , (float) Math.toRadians(displVec.y * 0.1f));
         }
-        if (window.isKeyPressed(GLFW_KEY_RIGHT)) {
-            camera.addRotation(0f, 0.01f);
+        if (window.getMouseInput().getScroll().y != 0){
+            projection.setFOV(projection.getFOV()-(window.getMouseInput().getScroll().y * 0.1f));
+            window.getMouseInput().setScroll(new Vector2f());
+        }
+
+
+        if (window.isKeyPressed(GLFW_KEY_R)){
+            press_r = true;
+        }
+        if (press_r){
+            float posX = camera.getPosition().x;
+            float posY = camera.getPosition().y;
+            float posZ = camera.getPosition().z;
+            camera.setPosition(-posX,-posY,-posZ);
+            camera.addRotation(0.0f,(float) Math.toRadians(move2));
+            camera.setPosition(posX,posY,posZ);
+            derajatCam_r += move2;
+            if(derajatCam_r >= 360.0){
+                press_r = false;
+                derajatCam_r = 0.0f;
+            }
+        }
+
+        if (window.isKeyPressed(GLFW_KEY_Q)){
+            camera.moveForward(1.7f);
+            camera.addRotation(0, (float)Math.toRadians(-1));
+            derajatCam_r+=-1;
+            camera.moveBackwards(1.7f);
         }
     }
 
@@ -89,7 +166,7 @@ public class Main {
             input();
 
             // code here
-            for (Object2d object: objectObj) {
+            for (Object object: objectObj) {
                 object.draw(camera, projection);
             }
 
@@ -102,7 +179,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Main().run();
     }
 }
